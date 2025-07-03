55ae7fcc55cc19880c58f715003e1e784244cea4
Simran Dalal <147065323+simrandalal@users.noreply.github.com>
2025-07-03 20:00:41 +0530

    Create NextPermutation.java

---
+++ src/Arrays/NextPermutation.java
@@ -0,0 +1,46 @@
+import java.util.Arrays; 
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {1, 2, 5, 4, 3};
+        
+        sol.nextPermutation(nums);
+        
+        System.out.println(Arrays.toString(nums)); 
+    }
+}
